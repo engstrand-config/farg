8f85966f7b81e6967092352a4c39a97eec2cda07
Fredrik Engstrand <fredrik@engstrand.nu>
2021-07-11 14:41:29 +0200

    chore: initial commit

---
+++ hall.scm
@@ -0,0 +1,25 @@
+(hall-description
+  (name "farg")
+  (prefix "")
+  (version "0.1")
+  (author "Fredrik Engstrand")
+  (copyright (2021))
+  (synopsis "farg is a simple system colorscheme generator")
+  (description "farg is a simple system colorscheme generator with sensible defaults")
+  (home-page "https://github.com/engstrand-config/farg")
+  (license gpl3+)
+  (dependencies `())
+  (files (libraries
+           ((scheme-file "farg") (directory "farg" ())))
+         (tests ((directory "tests" ())))
+         (programs ((directory "scripts" ())))
+         (documentation
+           ((org-file "README")
+            (symlink "README" "README.org")
+            (text-file "HACKING")
+            (text-file "COPYING")
+            (directory "doc" ((texi-file "farg")))))
+         (infrastructure
+           ((scheme-file "guix")
+            (text-file ".gitignore")
+            (scheme-file "hall")))))
