b8b291e5ef2b7403f2ca3c291f9aaf08fb5b2f6b
Fredrik Engstrand <fredrik@engstrand.nu>
2022-02-10 18:56:11 +0100

    feat: colorscheme: add hsl to rgba and hex conversions

--- farg/colorscheme.scm
+++ farg/colorscheme.scm
@@ -21,7 +21,14 @@
             hex->hsl
             hex->rgba
             hex->luminance
+            rgba->hex
+            rgba->hsl
+            hsl->hex
+            hsl->rgba
             contrast
+            adjust-luminance
+            lighten
+            darken
 
             colors->colorscheme
             generate-colorscheme
@@ -114,7 +121,7 @@
        (primary-text (assoc-ref colors 8))
        (secondary-text (assoc-ref colors 9)))))
 
-(define* (hex->rgba str #:key (alpha? #t))
+(define* (hex->rgba str #:key (alpha? #f))
   "Converts a hex color STR into its RGBA color representation.
 If the hex color does not specify the alpha, it will default to 100%."
   (define (split-rgb acc hex)
@@ -132,15 +139,39 @@ If the hex color does not specify the alpha, it will default to 100%."
          (if has-alpha? (cons 1.0 rgb) rgb)
          (if has-alpha? (list-tail rgb 1) rgb)))))
 
-(define* (rgb->hsl rgb)
-  "Converts RGB into its HSL color representation."
+(define* (hex->hsl hex)
+  "Converts a hex color HEX into its HSL color representation.
+Conversion of black and white will result in a hue of 0% (undefined)."
+  (rgba->hsl (hex->rgba hex)))
+
+;; Based on formula at https://www.myndex.com/WEB/LuminanceContrast.
+(define* (hex->luminance hex)
+  "Calculates the luminance of hex color HEX."
+  (apply + (map (lambda (pair) (* (car pair) (expt (cadr pair) 2.2)))
+                (zip '(0.2126 0.7152 0.0722)
+                     (hex->rgba hex)))))
+
+(define* (rgba->hex rgba #:key (alpha? #f))
+  "Converts RGBA into its hex color representation."
+  (fold
+   (lambda (v acc)
+     (string-append
+      acc
+      (format #f "~2,'0x" (bounded 0 255 (inexact->exact (round (* v 255)))))))
+   "#"
+   (if (and alpha? (= (length rgba) 3))
+       (append rgba '(1.0))
+       rgba)))
+
+(define* (rgba->hsl rgba)
+  "Converts RGBA into its HSL color representation."
   (define (safe-division x1 x2 denom)
     (if (= denom 0.0)
         0.0
         (/ (- x1 x2) denom)))
 
-  (let* ((c-min (apply min rgb))
-         (c-max (apply max rgb))
+  (let* ((c-min (apply min rgba))
+         (c-max (apply max rgba))
          (lum (/ (+ c-min c-max) 2))
          (sat (if (<= lum 0.5)
                   (safe-division c-max c-min (+ c-max c-min))
@@ -150,38 +181,56 @@ If the hex color does not specify the alpha, it will default to 100%."
                   ;; Hue is undefined in cases where the denominator is 0.
                   0.0
                   (* 60.0
-                     (match (list-index (lambda (v) (eq? v c-max)) rgb)
+                     (match (list-index (lambda (v) (eq? v c-max)) rgba)
                        ;; Red
-                       (0 (safe-division (list-ref rgb 1)
-                                         (list-ref rgb 2)
+                       (0 (safe-division (list-ref rgba 1)
+                                         (list-ref rgba 2)
                                          hue-denom))
                        ;; Green
-                       (1 (+ (safe-division (list-ref rgb 2)
-                                            (list-ref rgb 0)
+                       (1 (+ (safe-division (list-ref rgba 2)
+                                            (list-ref rgba 0)
                                             hue-denom)
                              2.0))
                        ;; Blue
-                       (2 (+ (safe-division (list-ref rgb 0)
-                                            (list-ref rgb 1)
+                       (2 (+ (safe-division (list-ref rgba 0)
+                                            (list-ref rgba 1)
                                             hue-denom)
                              4.0)))))))
     `(,(if (negative? hue) (+ hue 360) hue) ,sat ,lum)))
 
-(define* (hex->hsl hex)
-  "Converts a hex color HEX into its HSL color representation.
-Conversion of black and white will result in a hue of 0% (undefined)."
-  (rgb->hsl (hex->rgba hex #:alpha? #f)))
+(define* (hsl->rgba hsl)
+  "Convert HSL into its RGBA color representation."
+  (define hue (list-ref hsl 0))
+  (define sat (list-ref hsl 1))
+  (define lum (list-ref hsl 2))
+
+  (define (normalize-rgb-value v)
+    (if (negative? v)
+        (+ v 1)
+        (if (> v 1) (- v 1) v)))
+
+  (if (= sat 0.0)
+      ;; Shade of grey, convert to RGB directly.
+      (map (lambda (v) (* v lum)) `(1.0 1.0 1.0))
+      (let* ((magic1 (if (< lum 0.5)
+                         (* lum (+ 1.0 sat))
+                         (- (+ lum sat) (* lum sat))))
+             (magic2 (- (* 2.0 lum) magic1))
+             (hue-norm (/ hue 360))
+             (tmp-r (normalize-rgb-value (+ hue-norm 0.3333)))
+             (tmp-g (normalize-rgb-value hue-norm))
+             (tmp-b (normalize-rgb-value (- hue-norm 0.3333))))
+        (map (lambda (v)
+               (cond
+                ((< (* 6 v) 1.0) (+ magic2 (* 6.0 v (- magic1 magic2))))
+                ((< (* 2 v) 1.0) magic1)
+                ((< (* 3 v) 2.0) (+ magic2 (* 6.0 (- 0.6666 v) (- magic1 magic2))))
+                (else magic2)))
+         (list tmp-r tmp-g tmp-b)))))
 
 (define* (hsl->hex hsl)
   "Converts HSL into its hex color representation."
-  hsl)
-
-;; Based on formula at https://www.myndex.com/WEB/LuminanceContrast.
-(define* (hex->luminance hex)
-  "Calculates the luminance of hex color HEX."
-  (apply + (map (lambda (pair) (* (car pair) (expt (cadr pair) 2.2)))
-                (zip '(0.2126 0.7152 0.0722)
-                     (hex->rgba hex #:alpha? #f)))))
+  (rgba->hex (hsl->rgba hsl)))
 
 ;; Based on https://github.com/protesilaos/modus-themes/blob/main/modus-themes.el.
 (define* (contrast c1 c2)
