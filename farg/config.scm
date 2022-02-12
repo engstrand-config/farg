29c8e47751a28a6ff430477cb22181bf41d321c1
Fredrik Engstrand <fredrik@engstrand.nu>
2022-02-12 09:58:13 +0100

    feat: allow generation of custom colors in colorscheme

--- farg/config.scm
+++ farg/config.scm
@@ -5,23 +5,35 @@
             farg-config
             <farg-config>
             farg-config?
+            farg-config-light?
             farg-config-backend
-            farg-config-saturation
             farg-config-wallpaper
-            farg-config-light?))
+            farg-config-saturation
+            farg-config-palette-getter))
+
+(define (maybe-palette-getter? proc)
+  (or (eq? proc #f)
+      (eq? (car (procedure-minimum-arity proc)) 2)))
 
 (define-configuration
   farg-config
   (light?
    (boolean #f)
    "If a light colorscheme should be generated.")
+  (backend
+   (string "wal")
+   "Colorscheme generator backend to use.")
   (wallpaper
    (maybe-string #f)
    "Path for wallpaper to use in colorscheme generation.")
   (saturation
    (number 1.0)
    "Colorscheme saturation.")
-  (backend
-   (string "wal")
-   "Colorscheme generator backend to use.")
+  (palette-getter
+   (maybe-palette-getter #f)
+   "Procedure that accepts two arguments, a @code{colorscheme} record and
+the generated pywal colors as a list of index/color pairs. The return value
+of this procedure will be used to set the @code{(palette)} field of the
+final @code{colorscheme} record. This can be used to generate more shades
+of the pywal-generated colors.")
   (no-serialization))
--- farg/provider.scm
+++ farg/provider.scm
@@ -1,5 +1,7 @@
 (define-module (farg provider)
   #:use-module (srfi srfi-1)
+  #:use-module (ice-9 match)
+  #:use-module (ice-9 exceptions)
   #:use-module (farg config)
   #:use-module (farg colorscheme)
   #:use-module (gnu services configuration)
@@ -10,6 +12,31 @@
             themed-services-home
             themed-services-system
             colorscheme-provider))
+
+(define* (make-colorscheme-accessor colorscheme)
+  "Creates a helper procedure for quickly accessing base and palette
+colors from a generated colorscheme.
+@example
+(define palette (make-colorscheme-accessor colorscheme))
+(palette 'background) ;; returns the color in background field of colorscheme
+(palette 'my-custom-palette-color) ;; returns color from palette alist
+@end example"
+  (lambda (name)
+    (match name
+      ('primary (colorscheme-primary colorscheme))
+      ('secondary (colorscheme-secondary colorscheme))
+      ('text (colorscheme-text colorscheme))
+      ('primary-text (colorscheme-primary-text colorscheme))
+      ('secondary-text (colorscheme-secondary-text colorscheme))
+      ('background (colorscheme-background colorscheme))
+      (_ (let ((color (assoc-ref (colorscheme-palette colorscheme) name)))
+           (if color
+               color
+               (raise-exception
+                (make-exception-with-message
+                 (string-append "farg: '"
+                                (symbol->string name)
+                                "' does not exist in your colorscheme.")))))))))
 
 (define-configuration
   themed-services
@@ -35,16 +62,18 @@
   (define new-colorscheme
     (colors->colorscheme (read-generated-colorscheme) config))
 
-  (define (generate-services provider new-colorscheme)
+  (define accessor (make-colorscheme-accessor new-colorscheme))
+
+  (define (generate-services provider)
     (cond
      ((list? provider)
       (if (every (lambda (x) (>= 1 (car (procedure-minimum-arity x))))
                  provider)
-          (map (lambda (s) (s new-colorscheme)) provider)
+          (map (lambda (s) (s new-colorscheme accessor)) provider)
           provider))
-     ((procedure? provider) (provider new-colorscheme))
+     ((procedure? provider) (provider new-colorscheme accessor))
      (else provider)))
 
   (themed-services
-     (home (generate-services home-services new-colorscheme))
-     (system (generate-services system-services new-colorscheme)))))
+     (home (generate-services home-services))
+     (system (generate-services system-services))))
