600b915f98694bf92122775f6c1349b83af617ae
Fredrik Engstrand <fredrik@engstrand.nu>
2023-04-09 22:12:24 +0200

    feat: add more accent colors to theme

--- farg/theme.scm
+++ farg/theme.scm
@@ -8,8 +8,14 @@
             farg-theme-fg
             farg-theme-bg
             farg-theme-bg-alt
-            farg-theme-accent
-            farg-theme-complementary
+            farg-theme-accent-0
+            farg-theme-accent-1
+            farg-theme-accent-2
+            farg-theme-accent-3
+            farg-theme-accent-4
+            farg-theme-accent-5
+            farg-theme-accent-6
+            farg-theme-accent-7
             farg-theme-alpha
             farg-theme-light?
             farg-theme-wallpaper
@@ -26,12 +32,30 @@
   (bg-alt
    (string "")
    "Alternative background color.")
-  (accent
+  (accent-0
    (string "")
-   "Accent color.")
-  (complementary
+   "Accent 0 color.")
+  (accent-1
    (string "")
-   "Complementary accent color.")
+   "Accent 1 color.")
+  (accent-2
+   (string "")
+   "Accent 2 color.")
+  (accent-3
+   (string "")
+   "Accent 3 color.")
+  (accent-4
+   (string "")
+   "Accent 4 color.")
+  (accent-5
+   (string "")
+   "Accent 5 color.")
+  (accent-6
+   (string "")
+   "Accent 6 color.")
+  (accent-7
+   (string "")
+   "Accent 7 color.")
   (alpha
    (number 1.0)
    "Default transparency for the theme (0-1).")
